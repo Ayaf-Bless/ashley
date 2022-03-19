54a76d03cdfaf0174e1380769230d941b08d7cbb
Bless Ambele <blessambel1@gmail.com>
2022-03-19 04:07:23 -0930

    implemetting the header

---
+++ components/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+
+function Header(props) {
+  return (
+    <div className={"header"}>
+      <div className={"container"}>
+        <div className={"row v-center space-between"}>
+          <div className={"logo"}>
+            <Link href={"/"}>
+              <a>Ashley.</a>
+            </Link>
+          </div>
+          <div className={"nav"}>
+            <span />
+            <span />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/header";
 
 export default function Home() {
-  return <h1>hello</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
