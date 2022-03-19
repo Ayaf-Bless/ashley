647a62dbc33ebc93d56a25a07a824632970e93da
Bless Ambele <blessambel1@gmail.com>
2022-03-19 13:17:55 -0930

    finishing up the banner style

--- components/banner.js
+++ components/banner.js
@@ -3,6 +3,26 @@ import Link from "next/link";
 import Image from "next/image";
 import arrow from "../public/assets/arrow-right.svg";
 
+const SvgArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="feather feather-arrow-right"
+    >
+      <line x1="5" y1="12" x2="19" y2="12"></line>
+      <polyline points="12 5 19 12 12 19"></polyline>
+    </svg>
+  );
+};
+
 function Banner(props) {
   return (
     <section className={"main"}>
@@ -10,16 +30,16 @@ function Banner(props) {
         <div className={"row"}>
           <h2 className={"line"}>
             <div className={"line"}>
-              <span>Faire en sorte que vos briellez</span>
+              <span>Faire en sorte que vous brilez</span>
             </div>
             <div className={"line"}>
-              <span>C'est notre mettier</span>
+              <span>C'est notre soucis</span>
             </div>
           </h2>
           <div className={"btn-row"}>
             <Link href={"/about"}>
               <a>
-                en savoir plus <Image src={arrow} />
+                en savoir plus <SvgArrow />
               </a>
             </Link>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/header";
 import Banner from "../components/banner";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
   return (
     <div>
       <Header />
