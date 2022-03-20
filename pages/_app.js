6edd21855fa60a07a02a2313958eca6c6cdd2956
Bless Ambele <blessambel1@gmail.com>
2022-03-19 21:20:58 -0930

    refactoring the code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import "../styles/App.scss";
+import Header from "../components/header";
+import { useEffect } from "react";
+import gsap from "gsap";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    gsap.to("body", 0, { css: { visibility: "visible" } });
+  }, []);
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,48 @@
 import gsap from "gsap";
-import Header from "../components/header";
 import Banner from "../components/banner";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Cases from "../components/cases";
 import IntroOverlay from "../components/introOverlay";
 
-export default function Home() {
-  useEffect(() => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-    //  Gsap  timeline
-    const tl = gsap.timeline();
-
-    //Prevent flashing
-
-    tl.to("body", 0, { css: { visibility: "visible" } });
-
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: "power4.out",
-      delay: 1,
-      skewY: 7,
-      stagger: {
-        amount: 0.3,
-      },
+const tl = gsap.timeline();
+const homeAnimation = (completeAnimation) => {
+  tl.from(".line span", 1.8, {
+    y: 100,
+    ease: "power4.out",
+    delay: 1,
+    skewY: 7,
+    stagger: {
+      amount: 0.3,
+    },
+  })
+    .to(".overlay-top", 1.6, { height: 0, ease: "expo.inOut", stagger: 0.4 })
+    .to(".overlay-bottom", 1.6, {
+      width: 0,
+      ease: "expo.inOut",
+      delay: -0.8,
+      stagger: 0.4,
     })
-      .to(".overlay-top", 1.6, { height: 0, ease: "expo.inOut", stagger: 0.4 })
-      .to(".overlay-bottom", 1.6, {
-        width: 0,
-        ease: "expo.inOut",
-        delay: -0.8,
-        stagger: 0.4,
-      })
-      .to(".intro-overlay", 0, { css: { display: "none" } })
-      .from(".case-image img", 1.6, {
-        scale: 1.4,
-        ease: "expo.inOut",
-        delay: -2,
-        stagger: 0.2,
-      });
+    .to(".intro-overlay", 0, { css: { display: "none" } })
+    .from(".case-image img", 1.6, {
+      scale: 1.4,
+      ease: "expo.inOut",
+      delay: -2,
+      stagger: 0.2,
+      onComplete: completeAnimation,
+    });
+};
+
+export default function Home() {
+  const [animationComplete, setAnimationComplete] = useState(false);
+  const completeAnimation = () => {
+    setAnimationComplete(true);
+  };
+  useEffect(() => {
+    homeAnimation(completeAnimation);
   }, []);
   return (
     <div>
-      <IntroOverlay />
-      <Header />
+      {!animationComplete ? <IntroOverlay /> : null}
       <Banner />
       <Cases />
     </div>
