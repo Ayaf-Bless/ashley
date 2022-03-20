b29e0c929409480dae76ebc17d3132804611a4ff
Bless Ambele <blessambel1@gmail.com>
2022-03-19 16:06:02 -0930

    wraping up the case function

--- components/banner.js
+++ components/banner.js
@@ -1,27 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
-import arrow from "../public/assets/arrow-right.svg";
-
-const SvgArrow = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      className="feather feather-arrow-right"
-    >
-      <line x1="5" y1="12" x2="19" y2="12"></line>
-      <polyline points="12 5 19 12 12 19"></polyline>
-    </svg>
-  );
-};
+import { RightArrow } from "../utils/svg-component";
 
 function Banner(props) {
   return (
@@ -39,7 +18,7 @@ function Banner(props) {
           <div className={"btn-row"}>
             <Link href={"/about"}>
               <a>
-                en savoir plus <SvgArrow />
+                en savoir plus <RightArrow />
               </a>
             </Link>
           </div>
--- components/cases.js
+++ components/cases.js
@@ -1,9 +1,58 @@
 import React from "react";
+import { RightArrow as CasesNext } from "../utils/svg-component";
+import { ArrowLeft as CasesPrev } from "../utils/svg-component";
+import Image from "next/image";
+
+const caseStudies = [
+  {
+    id: 1,
+    subtitle: "Coiffure & maquillage",
+    title: "Pour tous vos besoins liés à la coiffure et au maquillage",
+    image: "hair",
+  },
+  {
+    id: 2,
+    subtitle: "Marriage",
+    title: "La location de robe de mariage et le maquillage",
+    image: "wedding",
+  },
+  {
+    id: 3,
+    subtitle: "Manicure",
+    title: "Pour vos soins de main et son embellissement",
+    image: "nails",
+  },
+];
 
 function Cases(props) {
   return (
-    <section>
-      <p>Hello</p>
+    <section className={"cases"}>
+      <div className={"container-fluid"}>
+        <div className={"cases-navigation"}>
+          <div className={"cases-arrow prev disabled"}>
+            <CasesPrev />
+          </div>
+          <div className={"cases-arrow next"}>
+            <CasesNext />
+          </div>
+        </div>
+        <div className={"row"}>
+          {caseStudies.map((caseItem) => (
+            <div className={"case"} key={caseItem.id}>
+              <div className={"case-details"}>
+                <span>{caseItem.subtitle}</span>
+                <h2>{caseItem.title}</h2>
+              </div>
+              <div className={"case-image"}>
+                <Image
+                  src={require(`../public/assets/ullistration/${caseItem.image}.jpg`)}
+                  alt={caseItem.title}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
