308e902c9bbf0bc88c0292b69e68c283a5c80993
Bless Ambele <blessambel1@gmail.com>
2022-03-20 09:23:35 -0930

    creating the 404 page

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+import { RightArrow } from "../utils/svg-component";
+
+function About() {
+  return (
+    <div className={"page"}>
+      <div className={"container"}>
+        <div className={"row"}>
+          Oupss...la page que vous cherchez n'existe pas 😏
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
