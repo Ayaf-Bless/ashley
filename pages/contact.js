5dd8f3b3001229cf7e38b3173df3e55be06cb0d9
Bless Ambele <blessambel1@gmail.com>
2022-03-22 16:35:06 -0930

    styling the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -9,10 +9,18 @@ function Contact() {
         <div className={"row"}>
           <div className="contact">
             <div className="contact__info">abc@gmail.com</div>
-            <form className="contact__form">
-              <input type={"text"} placeholder="Ton nom" />
-              <input type={"email"} placeholder="Ton email" />
-              <textarea placeholder="Ton message" />
+            <form className="contact__form" autoComplete="off">
+              <p className="para">
+                Ecrivez nous, et On vous repondra aussi vite que possible
+              </p>
+              <input type={"text"} placeholder="Ton nom" className="fields" />
+              <input
+                type={"email"}
+                placeholder="Ton email"
+                className="fields"
+                autoComplete="new password"
+              />
+              <textarea placeholder="Ton message" className="fields" />
               <button>Envoyer</button>
             </form>
           </div>
