4a4acbf113f157efe05e58cf38411f14609f5857
Bless Ambele <blessambel1@gmail.com>
2022-03-24 05:47:11 -0930

    adding the send email function

--- pages/contact.js
+++ pages/contact.js
@@ -1,26 +1,73 @@
-import React from "react";
+import React, { useRef } from "react";
 import Link from "next/link";
 import { RightArrow } from "../utils/svg-component";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_3pz7abp",
+        "template_jzk50dh",
+        form.current,
+        "ZWdlX-m5Px-VM3K3U"
+      )
+      .then(
+        (result) => {
+          alert("votre message a été envoyé avec succès");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <div className={"page App"}>
       <div className={"container"}>
         <div className={"row"}>
           <div className="contact">
             <div className="contact__info">abc@gmail.com</div>
-            <form className="contact__form" autoComplete="off">
+            <form
+              className="contact__form"
+              autoComplete="off"
+              onSubmit={sendEmail}
+              ref={form}
+            >
               <p className="para">
                 Ecrivez nous, et On vous repondra aussi vite que possible
               </p>
-              <input type={"text"} placeholder="Ton nom" className="fields" />
+              <input
+                type={"text"}
+                placeholder="Suject"
+                className="fields"
+                name="user_subject"
+                required
+              />
+              <input
+                type={"text"}
+                placeholder="Ton nom"
+                className="fields"
+                name="user_name"
+                required
+              />
               <input
                 type={"email"}
                 placeholder="Ton email"
                 className="fields"
                 autoComplete="new password"
+                name="user_email"
+                required
               />
-              <textarea placeholder="Ton message" className="fields" />
+              <textarea
+                placeholder="Ton message"
+                className="fields"
+                name="message"
+                required
+              />
               <button>Envoyer</button>
             </form>
           </div>
