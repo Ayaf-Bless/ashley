36c831842078f80a91ba56358d947b1e6b54837f
Bless Ambele <blessambel1@gmail.com>
2022-03-28 20:20:32 -0930

    remaking the show-case page

--- pages/show-case.js
+++ pages/show-case.js
@@ -2,19 +2,6 @@ import React from "react";
 import Loadable from "react-loadable";
 import Head from "next/head";
 
-const loader = () => (
-  <div className={"page App"}>
-    <div className={"container"}>
-      <div className={"row"}>Loading...</div>
-    </div>
-  </div>
-);
-//
-const HomeLazy = Loadable({
-  loader: () => import("../components/show-case"),
-  loading: loader,
-});
-
 const Index = () => {
   return (
     <>
